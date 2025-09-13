3ea780c77edd249a43ecf4a79218098237001e34
Carson Stone <carson.stone123@gmail.com>
2025-09-13 15:46:31 -0400

    add default plugins

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
 fn main() {
-  App::new().run();
+  App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, hello_world)
+    .run();
+}
+
+fn hello_world() {
+  println!("hello world!");
 }
