421ad32c6947c9db1c1611bb4f989db61aa19f1d
Carson Stone <carson.stone123@gmail.com>
2025-09-14 17:15:32 -0400

    add some text to the screen

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{color::palettes::css::GOLD, prelude::*};
+
+#[derive(Resource)]
+struct UiFont(Handle<Font>);
 
 #[derive(Component)]
 struct GameCamera;
@@ -6,11 +9,44 @@ struct GameCamera;
 fn main() {
   App::new()
     .add_plugins(DefaultPlugins)
-    .add_systems(Startup, hello_world)
+    // set the global default clear color (background color for cameras)
+    .insert_resource(ClearColor(Color::linear_rgb(0.9, 0.3, 0.6)))
+    .add_systems(Startup, (load_ui_font, setup, (write_text)).chain())
     .run();
 }
 
-fn hello_world(mut commands: Commands) {
-  println!("hello world!");
+fn setup(mut commands: Commands) {
+  println!("setting up game");
+
+  // setup camera
   commands.spawn((Camera2d::default(), GameCamera));
+}
+
+fn load_ui_font(mut commands: Commands, asset_server: Res<AssetServer>) {
+  let handle: Handle<Font> = asset_server.load("fonts/font.ttf");
+
+  // we can store the handle in a resource:
+  //  - to prevent the asset from being unloaded
+  //  - if we want to use it to access the asset later
+  commands.insert_resource(UiFont(handle));
+}
+
+fn write_text(mut commands: Commands, font_handle: Res<UiFont>) {
+  // add text to screen
+  commands.spawn((
+    Text::new("hello\nbevy!"),
+    TextFont {
+      // This font is loaded and will be used instead of the default font.
+      font: font_handle.0.clone(),
+      font_size: 36.0,
+      ..default()
+    },
+    TextColor(GOLD.into()),
+    Node {
+      position_type: PositionType::Absolute,
+      top: Val::Px(5.0),
+      left: Val::Px(5.0),
+      ..default()
+    },
+  ));
 }
