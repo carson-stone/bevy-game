b5935ed38f778e2e395eb40a6cd7e4b2005b1729
Carson Stone <carson.stone123@gmail.com>
2025-12-14 17:08:10 -0500

    make debug logging better, setting log level to debug for the game

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use bevy::prelude::*;
+use bevy::{log::LogPlugin, prelude::*};
 use bevy_game::GamePluginGroup;
 
 fn main() {
   App::new()
-    .add_plugins(DefaultPlugins)
+    .add_plugins(DefaultPlugins.set(LogPlugin {
+      filter: "info,wgpu_core=warn,wgpu_hal=warn,bevy_game=debug".into(),
+      level: bevy::log::Level::DEBUG,
+      ..default()
+    }))
     .add_plugins(GamePluginGroup)
     .run();
 }
