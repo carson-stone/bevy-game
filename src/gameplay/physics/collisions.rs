bbf4dc05c977417023cec9769e59a3925cc70501
Carson Stone <carson.stone123@gmail.com>
2025-12-14 16:41:16 -0500

    refactor collisions

--- src/gameplay/physics/collisions.rs
+++ src/gameplay/physics/collisions.rs
@@ -3,7 +3,6 @@ use bevy::{
   math::bounding::{Aabb2d, BoundingVolume, IntersectsVolume},
   prelude::*,
 };
-use std::collections::HashSet;
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum Collision {
@@ -44,51 +43,34 @@ fn determine_collision(box_a: Aabb2d, box_b: Aabb2d) -> Option<Collision> {
 
 pub fn check_for_collisions(
   mut collision_event_writer: EventWriter<CollisionEvent>,
-  collider_query_a: Query<(Entity, &Transform, Option<&Wall>), With<Collider>>,
-  collider_query_b: Query<(Entity, &Transform, Option<&Wall>), With<Collider>>,
+  colliders_query: Query<(Entity, &Transform, Option<&Wall>), With<Collider>>,
 ) {
-  let mut entities_colliding: HashSet<(Entity, Entity)> = HashSet::new();
+  // `iter_combinations` will iterate over all unique pairs of entities (skipping self-pairs)
+  for [collider_a, collider_b] in colliders_query.iter_combinations() {
+    let (entity_a, transform_a, maybe_wall_a) = collider_a;
+    let (entity_b, transform_b, maybe_wall_b) = collider_b;
 
-  for (entity_a, transform_a, maybe_wall_a) in &collider_query_a {
-    for (entity_b, transform_b, maybe_wall_b) in &collider_query_b {
-      // prevent collisions between a transform and itself
-      if entity_a == entity_b {
-        continue;
-      }
+    // prevent collisions between walls
+    if maybe_wall_a.is_some() && maybe_wall_b.is_some() {
+      continue;
+    }
 
-      // prevent collisions between walls
-      if maybe_wall_a.is_some() && maybe_wall_b.is_some() {
-        continue;
-      }
+    // check for a collision
+    let collision = determine_collision(
+      Aabb2d::new(
+        transform_a.translation.truncate(),
+        transform_a.scale.truncate() / 2.0,
+      ),
+      Aabb2d::new(
+        transform_b.translation.truncate(),
+        transform_b.scale.truncate() / 2.0,
+      ),
+    );
 
-      // prevent double-processing of collisions
-      if entities_colliding.contains(&(entity_a, entity_b))
-        || entities_colliding.contains(&(entity_b, entity_a))
-      {
-        continue;
-      }
-
-      // check for a collision
-      let collision = determine_collision(
-        Aabb2d::new(
-          transform_a.translation.truncate(),
-          transform_a.scale.truncate() / 2.0,
-        ),
-        Aabb2d::new(
-          transform_b.translation.truncate(),
-          transform_b.scale.truncate() / 2.0,
-        ),
-      );
-
-      // handle collision
-      if collision.is_some() {
-        // keep track of collisions so we don't double-process them
-        entities_colliding.insert((entity_a, entity_b));
-        entities_colliding.insert((entity_b, entity_a));
-
-        // emit a collision event
-        collision_event_writer.write(CollisionEvent(entity_a, entity_b, collision.unwrap()));
-      }
+    // handle collision
+    if collision.is_some() {
+      // emit a collision event
+      collision_event_writer.write(CollisionEvent(entity_a, entity_b, collision.unwrap()));
     }
   }
 }
